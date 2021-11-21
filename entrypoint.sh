172682d8424a01cc3577b8e144c9ecf08ff08e5a
Hans Hazairi <hanshazairi@gmail.com>
2021-11-21 17:46:25 +0800

    feat: optional arg to exclude 1 directory

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
-find . -type f \( -name '*.c' -o -name '*.h' \) | xargs norminette
-if find . -type f \( -name '*.c' -o -name '*.h' \) | xargs norminette | grep -Eq '^(Error|Warning)'; then
-  exit 1
+if [ -z "$1" ]; then
+  find . -type f \( -name '*.c' -o -name '*.h' \) | xargs norminette
+  if find . -type f \( -name '*.c' -o -name '*.h' \) | xargs norminette | grep -Eq '^(Error|Warning)'; then
+    exit 1
+  fi
+else
+  find . -path $1 -prune -o -type f \( -name '*.c' -o -name '*.h' \) -print | xargs norminette
+  if find . -path $1 -prune -o -type f \( -name '*.c' -o -name '*.h' \) -print | xargs norminette | grep -Eq '^(Error|Warning)'; then
+    exit 1
+  fi
 fi
